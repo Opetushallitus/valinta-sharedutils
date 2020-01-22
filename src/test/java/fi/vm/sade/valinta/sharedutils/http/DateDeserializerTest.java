e84c59dd26359309a59725ed95d4f11100193e3f
Esko Lahti <esko.lahti@reaktor.com>
2020-01-22 10:32:10 +0200

    Serialisoidaan päivämäärät ISO8601 -formaatin mukaan

--- src/test/java/fi/vm/sade/valinta/sharedutils/http/DateDeserializerTest.java
+++ src/test/java/fi/vm/sade/valinta/sharedutils/http/DateDeserializerTest.java
@@ -1,18 +1,14 @@
 package fi.vm.sade.valinta.sharedutils.http;
 
+import com.google.gson.*;
 import fi.vm.sade.valinta.sharedutils.http.DateDeserializer;
 
 import java.lang.reflect.Type;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,4 +28,30 @@ public class DateDeserializerTest {
         }
     }
 
+    @Test
+    public void serializesDate() {
+        Gson GSON = DateDeserializer
+                .gsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                .create();
+        final String str = GSON.toJson(new DateObject(new Date(1579680865000L)), DateObject.class);
+        Assert.assertNotNull(str);
+        Assert.assertEquals("{\"date\":\"2020-01-22T10:14:25Z\"}", str);
+    }
+
+    private static class DateObject {
+        private Date date;
+
+        public DateObject(Date date) {
+            this.date = date;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+    }
 }
