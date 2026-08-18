4ed1a98b60c40fd524cbc73e211301aa361ac1c6
Topias Heinonen <topias.heinonen@gofore.com>
2026-08-18 16:23:45 +0300

    OY-5547 Poistetaan CAS-riippuvuus
    Riippuvuutta käytetään vain yhden POJOn tuomiseen. Sen sijaan lisätään
    koodiin suoraan minimaalinen Principal-toteutus.

--- src/test/java/fi/vm/sade/valinta/sharedutils/FakeAuthenticationInitialiser.java
+++ src/test/java/fi/vm/sade/valinta/sharedutils/FakeAuthenticationInitialiser.java
@@ -1,6 +1,5 @@
 package fi.vm.sade.valinta.sharedutils;
 
-import org.apereo.cas.client.authentication.SimplePrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.TestingAuthenticationToken;
